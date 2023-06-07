e3a77f1578ebedce11cb542fce62be4f6376eb86
AnitaUprise <anita@uprise.us>
2023-06-07 14:40:17 -0700

    adding state

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 
-export default function ExpenseForm() {
+export default function ExpenseForm(props) {
     const [enteredTitle, setEnteredTitle] = useState('');
     const [enteredAmount, setEnteredAmount] = useState('');
     const [enteredDate, setEnteredDate] = useState('');
@@ -35,19 +35,34 @@ export default function ExpenseForm() {
         //     enteredDate: event.target.value,  
         // })
     };
-    return <form>
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        const expenseData = {
+            title: enteredTitle,
+            amount: enteredAmount,
+            date: new Date(enteredDate)
+        };
+
+        props.onSaveExpenseData(expenseData);
+        setEnteredAmount('');
+        setEnteredTitle('');
+        setEnteredDate('');
+    };
+    return <form onSubmit={handleSubmit}>
         <div className="new-expense__controls">
             <div className="new-expense__control">
                 <label>Title</label>
-                <input type="text" onChange={handleTitleChange}/>
+                <input type="text" value={enteredTitle} onChange={handleTitleChange}/>
             </div>
             <div className="new-expense__control">
                 <label>Amount</label>
-                <input type="number" min="0.01" step="0.01" onChange={handleAmountChange}/>
+                <input type="number" min="0.01" step="0.01" value={enteredAmount} onChange={handleAmountChange}/>
             </div>
             <div className="new-expense__control">
                 <label>Date</label>
-                <input type="date" min="2019-01-01" max="2022-12-31" onChange={handleDateChange}/>
+                <input type="date" min="2019-01-01" max="2022-12-31" value={enteredDate} onChange={handleDateChange}/>
             </div>
         </div>
         <div className="new-expense__actions">
--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,8 +1,17 @@
  import './NewExpense.css';
  import ExpenseForm from './ExpenseForm';
  
- export default function NewExpense() {
-    return <div className="new-expense">
-        <ExpenseForm />
+ export default function NewExpense(props) {
+    const handleSaveExpenseData = (enteredExpenseData) => {
+        const expenseData = {
+            ...enteredExpenseData,
+            id: Math.random().toString()
+        };
+        props.onAddExpense(expenseData);
+    };
+    return (
+    <div className="new-expense">
+        <ExpenseForm onSaveExpenseData={handleSaveExpenseData} />
     </div>
+    );
 }
